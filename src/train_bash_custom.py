1d3355ea932c87257fc9efbd43d76591a0ffcdad
Li_Zhongwei <Li_zhongwei@imda.gov.sg>
2024-04-26 15:32:48 +0800

    sync changes from AWS  https://github.com/billcai/llama-factory-modified

---
+++ src/train_bash_custom.py
@@ -0,0 +1,21 @@
+import os, subprocess
+
+TRAIN_ARGS = os.getenv('TRAIN_ARGS')
+TRAIN_ARGS_ADD = os.getenv('TRAIN_ARGS_ADD')
+DEEPSPEED=os.getenv('DEEPSPEED','true')
+NUM_GPUS = os.getenv('NUM_GPUS','1')
+if DEEPSPEED=='true':
+    subprocess.run(
+        ["deepspeed", "--num_gpus", NUM_GPUS, "/app/src/train_bash.py"]+
+        ["--deepspeed", "/app/examples/deepspeed/ds_z3_config.json"]+
+        TRAIN_ARGS.split(' ')+
+        TRAIN_ARGS_ADD.split(' '),
+        check=True
+        )
+else:
+    subprocess.run(
+        ["python", "src/train_bash.py"]+
+        TRAIN_ARGS.split(' ')+
+        TRAIN_ARGS_ADD.split(' '),
+        check=True
+        )
