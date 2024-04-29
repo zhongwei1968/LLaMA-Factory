76aff6cb0ab9bb4a6bb6ac7f3e93dcff711e23fe
Li_Zhongwei <Li_zhongwei@imda.gov.sg>
2024-04-29 14:52:26 +0800

    add accelerate run

--- src/train_bash_custom.py
+++ src/train_bash_custom.py
@@ -2,13 +2,12 @@ import os, subprocess
 
 TRAIN_ARGS = os.getenv('TRAIN_ARGS')
 TRAIN_ARGS_ADD = os.getenv('TRAIN_ARGS_ADD')
-DEEPSPEED=os.getenv('DEEPSPEED','false')
-ACCELERATE=os.getenv('ACCELERATE','true')
+run_com = os.getenv('RUN_CMD','accelerate') # accelerate or deepspeed
 NUM_GPUS = os.getenv('NUM_GPUS','1')
 
 print(os.listdir('/opt/ml/input/data/training'))
 
-if DEEPSPEED=='true':
+if run_com=='deepspeed':
     subprocess.run(
         ["deepspeed", "--num_gpus", NUM_GPUS, "/app/src/train_bash.py"]+
         ["--deepspeed", "/app/examples/deepspeed/ds_z3_config.json"]+
@@ -16,7 +15,7 @@ if DEEPSPEED=='true':
         TRAIN_ARGS_ADD.split(),
         check=True
         )
-elif ACCELERATE=='true':
+elif run_com=='accelerate':
     subprocess.run(
         ["accelerate", "launch", "--config_file", "/app/examples/accelerate/single_config.yaml",
          "/app/src/train_bash.py"]+
