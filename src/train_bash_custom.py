953722d418436e5a50bb993897916893f40f3105
Zhongwei Li <li_zhongwei@imda.gov.sg>
2025-09-30 15:00:31 +0800

    add pipeline training and configs

---
+++ src/train_bash_custom.py
@@ -0,0 +1,34 @@
+import os, subprocess
+
+TRAIN_ARGS = os.getenv('TRAIN_ARGS')
+TRAIN_ARGS_ADD = os.getenv('TRAIN_ARGS_ADD')
+run_com = os.getenv('RUN_CMD','accelerate') # accelerate or deepspeed
+NUM_GPUS = os.getenv('NUM_GPUS','1')
+
+print(os.listdir('/opt/ml/input/data/training'), flush=True)
+
+if run_com=='deepspeed':
+    print("running deepspeed", flush=True)
+    subprocess.run(
+        ["deepspeed", "--num_gpus", NUM_GPUS, "/app/src/train.py"]+
+        ["--deepspeed", "/app/examples/deepspeed/ds_z3_config.json"]+
+        TRAIN_ARGS.split()+
+        TRAIN_ARGS_ADD.split(),
+        check=True
+        )
+elif run_com=='accelerate':
+    print("running accelerate", flush=True)
+    subprocess.run(
+        ["accelerate", "launch", "--config_file", "/app/examples/accelerate/single_config_8.yaml",
+         "/app/src/train.py"]+
+        TRAIN_ARGS.split()+
+        TRAIN_ARGS_ADD.split(),
+        check=True
+        )
+else:
+    subprocess.run(
+        ["python", "src/train.py"]+
+        TRAIN_ARGS.split()+
+        TRAIN_ARGS_ADD.split(),
+        check=True
+        )
