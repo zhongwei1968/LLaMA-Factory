ff96f1c703eea9fabd1ad7c02421dd5355075434
Li_Zhongwei <li_zhongwei@imda.gov.sg>
2025-10-07 21:10:55 +0800

    support torchrun by default

--- src/train_bash_custom.py
+++ src/train_bash_custom.py
@@ -2,7 +2,7 @@ import os, subprocess
 
 TRAIN_ARGS = os.getenv('TRAIN_ARGS')
 TRAIN_ARGS_ADD = os.getenv('TRAIN_ARGS_ADD')
-run_com = os.getenv('RUN_CMD','accelerate') # accelerate or deepspeed
+run_com = os.getenv('RUN_CMD','torchrun') # torchrun accelerate or deepspeed
 NUM_GPUS = os.getenv('NUM_GPUS','1')
 
 print(os.listdir('/opt/ml/input/data/training'), flush=True)
@@ -19,12 +19,18 @@ if run_com=='deepspeed':
 elif run_com=='accelerate':
     print("running accelerate", flush=True)
     config_file = "/app/examples/accelerate/single_config_8.yaml"
-    if 'gemma' in TRAIN_ARGS or 'gemma' in TRAIN_ARGS_ADD:
-        config_file = "/app/examples/accelerate/fsdp_config_gemma3_8.yaml"
-
     subprocess.run(
         ["accelerate", "launch", "--config_file", config_file,
          "/app/src/train.py"]+
+        TRAIN_ARGS.split()+
+        TRAIN_ARGS_ADD.split(),
+        check=True
+        )
+elif run_com=='torchrun':
+    print("running torchrun", flush=True)
+    nproc_per_node = '--nproc_per_node=' + str(NUM_GPUS)
+    subprocess.run(
+        ["torchrun", nproc_per_node, "/app/src/train.py"]+
         TRAIN_ARGS.split()+
         TRAIN_ARGS_ADD.split(),
         check=True
